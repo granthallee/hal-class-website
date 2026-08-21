83223d23b6f53f321c4ad481660c660aed55d11e
Grant W Hallee <grantwhallee@gmail.com>
2026-08-21 13:20:17 -0700

    template 1

---
+++ script.js
@@ -0,0 +1,42 @@
+// Main tab switching
+document.querySelectorAll("#nav li").forEach(function(navEl) {
+    navEl.onclick = function() { 
+        toggleMainTab(this.id, this.dataset.target); 
+    }
+});
+
+function toggleMainTab(selectedNav, targetId) {
+    // Update main tab active states
+    var navEls = document.querySelectorAll("#nav li");
+    navEls.forEach(function(navEl) {
+        if (navEl.id == selectedNav) {
+            navEl.classList.add("is-active");
+        } else {
+            navEl.classList.remove("is-active");
+        }
+    });
+
+    // Show/hide main tab panes
+    var tabs = document.querySelectorAll(".tab-pane");
+    tabs.forEach(function(tab) {
+        if (tab.id == targetId) {
+            tab.classList.add("is-active");
+        } else {
+            tab.classList.remove("is-active");
+        }
+    });
+}
+
+// Sub-tab switching
+function setupSubTabs(subnavId) {
+    document.querySelectorAll(`#${subnavId} li`).forEach(function(navEl) {
+        navEl.onclick = function() {
+            toggleSubTab(subnavId, this.id, this.dataset.target);
+        }
+    });
+}
+
+function toggleSubTab(subnavId, selectedNav, targetId) {
+    // Update sub-tab active states
+    var navEls = document.querySelectorAll(`#${subnavId} li`);
+    navEls.forEach(function
